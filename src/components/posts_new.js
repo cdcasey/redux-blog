64dcbfb01fe7d26c9ae21821e833ea5387d108b0
Chris Casey <cdcasey@gmail.com>
2018-05-20 18:55:16 -0500

    Post creation works

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createPost } from '../actions';
 
 class PostsNew extends Component {
     renderField(field) {
@@ -39,9 +41,9 @@ class PostsNew extends Component {
     }
 
     onSubmit(values) {
-        console.log(values);
-        console.log(this.props);
-
+        this.props.createPost(values, () => {
+            this.props.history.push('/');
+        });
     }
 
     render() {
@@ -93,4 +95,6 @@ function validate(values) {
 export default reduxForm({
     validate,
     form: 'PostsNewForm'
-})(PostsNew);
+})(
+    connect(null, { createPost })(PostsNew)
+);
