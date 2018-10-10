349033ae39eaf45d1104c85c04062a9dcafc4b5e
Chris Casey <cdcasey@gmail.com>
2018-10-10 13:37:39 -0500

    Basic structure with redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+// import * as serviceWorker from './serviceWorker';
+import { BrowserRouter, Route } from 'react-router-dom';
+import promise from 'redux-promise';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './index.css';
+import reducers from './reducers';
+import PostsIndex from './components/posts_index';
+
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
+ReactDOM.render(
+    <Provider store={createStoreWithMiddleware(reducers)}>
+        <BrowserRouter>
+            <div>
+                <Route path="/" component={PostsIndex} />
+            </div>
+        </BrowserRouter>
+    </Provider>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
