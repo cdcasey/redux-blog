9fa54e9deaac28a491087fb066e6b8a095508eef
Chris Casey <cdcasey@gmail.com>
2018-10-12 13:40:42 -0500

    Post displays

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,11 +1,45 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { fetchPost } from '../actions';
 
 // A user could bookmark the show page, so this component
 // needs to fetch its own data
 class PostsShow extends React.Component {
+    componentDidMount() {
+        // Match is a prop provided by react-router
+        const { id } = this.props.match.params;
+        this.props.fetchPost(id).catch(console.error);
+    }
+
     render() {
-        return <div>Posts Show!</div>;
+        const { post } = this.props;
+
+        if (!post) {
+            return <div>Loading...</div>;
+        }
+
+        return (
+            <div>
+                <Link to="/">Back to index</Link>
+                <h3>{post.title}</h3>
+                <h6>Categories: {post.categories}</h6>
+                <p>{post.content}</p>
+            </div>
+        );
     }
 }
 
-export default PostsShow;
+// Gets just the posts from the application state object
+// ownProps === this.props of PostsShow
+function mapStateToProps({ posts }, ownProps) {
+    // Could just be return { posts };
+    // Uses the component's props to get the id from the URL
+    // and get the corresponding post from the application state
+    return { post: posts[ownProps.match.params.id] };
+}
+
+export default connect(
+    mapStateToProps,
+    { fetchPost }
+)(PostsShow);
