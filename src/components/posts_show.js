c95329d6a40f612014d9ac4a11dbe0079ddc11df
Chris Casey <cdcasey@gmail.com>
2018-10-12 12:04:01 -0500

    Pieces for showing a post

---
+++ src/components/posts_show.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// A user could bookmark the show page, so this component
+// needs to fetch its own data
+class PostsShow extends React.Component {
+    render() {
+        return <div>Posts Show!</div>;
+    }
+}
+
+export default PostsShow;
